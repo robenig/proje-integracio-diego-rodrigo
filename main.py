7562d9548e5c9c14a006473502345826c49cb755
46417151 <168165745+46417151@users.noreply.github.com>
2024-05-07 09:43:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,35 +5,32 @@ import os
 def menu_configuracion(config):
     while True:
         print("\n--- Menú de Configuración ---")
-        print("1. Ver Configuración")
+        print("1. Crear Nuevo Archivo de Configuración")
         print("2. Cargar Configuración")
-        print("3. Cambiar Parámetros")
-        print("4. Guardar Configuración")
-        print("5. Crear Nuevo Archivo de Configuración")
-        print("6. Crear directorios")
+        print("3. Ver Configuración")
+        print("4. Cambiar Parámetros")
+        print("5. Crear directorios")
         print("0. Salir")
         opcion = input("Selecciona una opción: ")
         if opcion == '1':
+            crear_configuracion()
+        elif opcion == '2':
+            cargar_configuracion()
+        elif opcion == '3':
             if config:
                 ver_configuracion(config)
             else:
                 print("No se ha cargado ninguna configuración.")
-        elif opcion == '2':
-            config = cargar_configuracion()
-        elif opcion == '3':
-            if config:
-                config = cambiar_parametros(config)
-            else:
-                print("No se ha cargado ninguna configuración.")
         elif opcion == '4':
             if config:
-                guardar_configuracion(config)
+                cambiar_parametros(config)
             else:
                 print("No se ha cargado ninguna configuración.")
         elif opcion == '5':
-            config = crear_configuracion()
-        elif opcion == '6':
-            crear_directorios(config)
+            if config:
+                crear_directorios(config)
+            else:
+                print("No se ha cargado ninguna configuración.")
         elif opcion == '0':
             break
         else:
@@ -66,14 +63,32 @@ def cargar_configuracion():
 
 
 def cambiar_parametros(config):
-    seccion = input("Introduce la sección del parámetro que quieres cambiar: ")
+    seccion = 'PARAMETROS'  # Sección fija para los parámetros
+    parametros = config[seccion]  # Obtener todos los parámetros de la sección
+    print("Parámetros disponibles para modificar:")
+    for parametro, valor in parametros.items():
+        print(f"{parametro}: {valor}")  # Mostrar el nombre del parámetro y su valor actual
+    
     parametro = input("Introduce el nombre del parámetro que quieres cambiar: ")
-    valor_actual = config.get(seccion, parametro)
-    print(f"El valor actual de {parametro} es: {valor_actual}")
-    nuevo_valor = input("Introduce el nuevo valor: ")
-    config.set(seccion, parametro, nuevo_valor)
-    print("¡Valor cambiado con éxito!")
+    if parametro in parametros:
+        valor_actual = parametros[parametro]
+        print(f"El valor actual de {parametro} es: {valor_actual}")  # Mostrar el valor actual del parámetro
+        nuevo_valor = input("Introduce el nuevo valor: ")
+        config.set(seccion, parametro, nuevo_valor)
+        print("¡Valor cambiado con éxito!")
+        
+        # Guardar la configuración actualizada en el archivo
+        archivo_nuevo = input("Introduce el nombre del archivo de configuración a actualizar: ")
+        confirmacion = input(f"¿Estás seguro que quieres guardar la configuración en {archivo_nuevo}? (S/N): ")
+        if confirmacion.upper() == 'S':
+            with open(archivo_nuevo, 'w') as configfile:
+                config.write(configfile)
+            print("¡Configuración guardada con éxito!")
+    else:
+        print("El parámetro ingresado no es válido.")
+    
     return config
+
 
 
 def crear_configuracion():
